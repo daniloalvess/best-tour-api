2f79a6f47889977c4f2837991ffed50b30cbbeb3
Danilo Alves <daniloalves.ti@gmail.com>
2019-09-02 17:20:57 -0300

    Alterando resposta

--- api.py
+++ api.py
@@ -9,7 +9,34 @@ class Tour(Resource):
     def post(self):
         data = request.get_json()
         locations = data["locations"]
-        return build_tours(locations), 200
+
+        if len(locations) == 0:
+            return {
+                "error": {
+                    "message": "Empty locations list"
+                },
+                "success": False
+            }, 412
+
+        return self.get_ordered_tour(locations), 200
+
+    def get_ordered_tour(self, locations):
+        tour_list = []
+        build_list = build_tours(locations)
+
+        for location_index in build_list:
+            tour_list.append(locations[location_index])
+
+        result = {
+            "data" : {
+                "keys_order" : build_list,
+                "tour": tour_list
+            },
+            "success": True
+        }
+
+        return result
+
 
 api.add_resource(Tour, "/tours/build")
 
